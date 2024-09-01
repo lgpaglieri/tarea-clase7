f19a65001164d637f0956207bf3593d86dfa1d16
Lucas Paglieri <lucas.paglieri@gmail.com>
2024-09-01 16:15:03 -0300

    completa-tarea1-clase8
    Se completó la primer parte de la tarea de la clase 8 (La tarea correspondiente a la página de carta a santa).

--- js-xmas-edition-master/js/main.js
+++ js-xmas-edition-master/js/main.js
@@ -1,25 +1,101 @@
-const ciudad=document.formulario.ciudad.value;
-function validarCiudad (ciudad){
-    if (ciudad===''){
-        return 'Para poder enviar el formulario es necesario seleccionar una ciudad'
-    }
-    return '';
+function validarCiudad(ciudad) {
+  if (ciudad === "") {
+    return "Para poder enviar el formulario es necesario seleccionar una ciudad";
+  }
+  return "";
 }
 
-const descripcionRegalo=document.formulario['descripcion-regalo'].value;
-function validarDescripcionRegalo (descripcionRegalo){
-    if (descripcionRegalo===''){
-        return 'Tenes que pedir algo si o si!'
-    }
-    return '';
+function validarDescripcionRegalo(descripcionRegalo) {
+  if (descripcionRegalo === "") {
+    return "Tenes que pedir algo si o si!";
+  }
+  return "";
 }
 
-const nombre=document.formulario.nombre.value;
-function validarNombre(nombre){
-    if (nombre.length===0){
-        return 'Este campo debe tener al menos 1 caracter';
-    }
-    if (nombre.length>50){
-        return 'Este campo debe tener menos de 50 caracteres';
-    }
+function validarNombre(nombre) {
+  if (nombre.length === 0) {
+    return "El campo nombre debe tener al menos un caracter";
+  }
+  if (nombre.length > 50) {
+    return "El campo nombre debe tener menos de 50 caracteres";
+  }
 }
+
+function validarFormulario(event) {
+  const $form = document.querySelector("#carta-a-santa");
+  const nombre = $form.nombre.value;
+  const ciudad = $form.ciudad.value;
+  const descripcionRegalo = $form["descripcion-regalo"].value;
+
+  const errorNombre = validarNombre(nombre);
+  const errorCiudad = validarCiudad(ciudad);
+  const errorDescripcionRegalo = validarDescripcionRegalo(descripcionRegalo);
+
+  const errores = {
+    nombre: errorNombre,
+    ciudad: errorCiudad,
+    "descripcion-regalo": errorDescripcionRegalo,
+  };
+  borrarListaErrores();
+
+  const esExito = manejarErrores(errores) === 0;
+  
+  if (esExito){
+  mostrarTextoExito();
+  ocultarFormulario();
+  redireccionarAWishlist();
+  }
+
+  event.preventDefault();
+}
+
+function borrarListaErrores(){
+    const $listaErrores=document.querySelectorAll('#errores>li')
+    for (let i=0;i<$listaErrores.length;i++){
+        $listaErrores[i].remove();
+    }
+  }
+
+function manejarErrores(errores) {
+  const keys = Object.keys(errores);
+  const $errores=document.querySelector('#errores');
+  let cantidadErrores=0;
+
+  keys.forEach(function (key) {
+    const error = errores[key];
+    if (error) {
+      $form[key].className = "error";
+      
+      const $error=document.createElement('li');
+      $error.innerText=error;
+      $errores.appendChild($error);
+
+      cantidadErrores++;
+
+    } else {
+      $form[key].className = "";
+    }
+  });
+  console.log(cantidadErrores);
+  return cantidadErrores;
+}
+
+function ocultarFormulario(){
+        $form.className="oculto";
+}
+
+function mostrarTextoExito(){
+    const $exito=document.querySelector('#exito');
+        $exito.className="";
+}
+
+function redireccionarAWishlist() {
+        setTimeout(function() {
+        window.location.href = "wishlist.html";
+        }, 5000);
+  }
+
+
+
+const $form = document.querySelector("#carta-a-santa");
+$form.onsubmit = validarFormulario;
--- js-xmas-edition-master/js/pruebas.js
+++ js-xmas-edition-master/js/pruebas.js
@@ -1,19 +1,19 @@
 function probarValidarNombre() {
   console.assert(
-      validarNombre('') === 'Este campo debe tener al menos 1 caracter',
+      validarNombre('') === 'El campo nombre debe tener al menos un caracter',
       'Validar nombre no validó que el nombre no sea vacío',
   );
 
   console.assert(
       validarNombre(
           '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') ===
-      'Este campo debe tener menos de 50 caracteres',
+      'El campo nombre debe tener menos de 50 caracteres',
       'Validar nombre no validó que el nombre sea menor a 50 caracteres',
   );
 }
 function probarValidarCiudad(){
     console.assert(
-        validarCiudad('Selecciona...')==='Para poder enviar el formulario es necesario seleccionar una ciudad',
+        validarCiudad('')==='Para poder enviar el formulario es necesario seleccionar una ciudad',
         'Validar ciudad no validó que no se haya seleccionado ninguna opción'
     )
 }
